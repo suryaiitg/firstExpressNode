efe499e330895c8013bae802c59ac685e615bab2
Surya <ssingh@innominds.com>
2016-06-18 20:55:47 +0530

    Node Server

---
+++ server.js
@@ -0,0 +1,16 @@
+express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(9000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+});
+
